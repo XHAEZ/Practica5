9ffc271b17d34efdb80c61e80a55c8acfd4c5233
Danny Vinicio Cobos Vega <danny.cobos@unl.edu.ec>
2023-07-06 21:58:19 -0500

    Fin

--- src/controller/DAO/BusDao.java
+++ src/controller/DAO/BusDao.java
@@ -1,4 +1,4 @@
-    /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -9,7 +9,6 @@ import controller.ed.lista.ListaEnlazada;
 import controller.ed.lista.exception.EmptyException;
 import controller.ed.lista.exception.PositionException;
 import java.io.IOException;
-import model.Bus;
 import model.Bus;
 
 /**
